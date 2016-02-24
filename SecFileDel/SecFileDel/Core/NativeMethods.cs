2c23d618efba3534dc5ae2f7a49dc0c8a5f78800
JordanHook <hookjordan@live.com>
2016-02-24 15:55:04 -0500

    test

---
+++ SecFileDel/SecFileDel/Core/NativeMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecFileDel.Core
+{
+    class NativeMethods
+    {
+        [Flags]
+        internal enum MoveFileFlags
+        {
+            None = 0,
+            ReplaceExisting = 1,
+            CopyAllowed = 2,
+            DelayUntilReboot = 4,
+            WriteThrough = 8,
+            CreateHardlink = 16,
+            FailIfNotTrackable = 32,
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool MoveFileEx(string lpExistingFileName, string lpNewFileName, MoveFileFlags dwFlags);
+    }
+}
---
+++ SecFileDel/SecFileDel/Core/SFD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;    
+
+namespace SecFileDel.Core
+{
+    class SFD
+    {
+        //Public properties 
+        public RemovalMode Mode { get { return _mode; } }
+        public int Rounds { get { return _rounds; } }
+
+        //Private properties 
+        private RemovalMode _mode; 
+        private int _rounds;
+
+        public enum RemovalMode
+        {
+            NormalDelete = 0, //regular built in delete methods...
+            RandomDelete, //overwrite the file with random data 
+            NullDelete //overwrite the file with null data 
+        }
+
+        /// <summary>
+        /// Create a new instance of the file deleter
+        /// </summary>
+        /// <param name="mode">The type of delete you would like to perform.</param>
+        /// <param name="rounds">The amount of times you would like to overwrite data(1, 3, 7, 11, etc)</param>
+        public SFD(RemovalMode mode, int rounds = 1)
+        {
+            //pass the parameters... 
+            this._mode = mode;
+            this._rounds = rounds; 
+        }
+
+
+
+    }
+}
